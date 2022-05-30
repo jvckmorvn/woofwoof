ef2293a2841184094f82b5d41174b9837097f54f
Jack Moran <jvckmorvn@gmail.com>
2022-05-30 17:09:06 +0100

    Enabled visitors to view home/landing page before signing in, and implemented dogs controller with index action (and 'get' route)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get	'/dogs', to: "pages#index"
 end
