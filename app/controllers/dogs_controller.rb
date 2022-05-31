dfa96fef88dbdb7c78df87542d9be92317d7a85a
Jack Moran <jvckmorvn@gmail.com>
2022-05-31 11:58:17 +0100

    Implemented add new dog feature

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -3,7 +3,11 @@ class DogsController < ApplicationController
   def index
     @dogs = Dog.all
   end
-  
+
+  def new
+    @dog = Dog.new
+  end
+
   def create
     @dog = Dog.new(dog_params)
     if @dog.save
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get	"dogs", to: "dogs#index"
+  get "dogs/new", to: "dogs#new"
   post "dogs", to: "dogs#create"
 end
