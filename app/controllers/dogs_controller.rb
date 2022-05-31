9a63aac8043867a917d260bab9658aef02a8273b
Saki Unno <unx2o.sk@gmail.com>
2022-05-31 17:27:45 +0100

    show view

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,6 @@
 class DogsController < ApplicationController
   def index
-      @dogs = Dog.all
+    @dogs = Dog.all
   end
 
   def create
@@ -14,6 +14,7 @@ class DogsController < ApplicationController
 
   def show
     @dog = Dog.find(params[:id])
+    @booking = Booking.new
   end
 
   private
