9fb2fb94ec6df79847b15806e7ee7fff8743c768
Saki Unno <unx2o.sk@gmail.com>
2022-06-02 17:16:38 +0100

    edit index page

--- db/schema.rb
+++ db/schema.rb
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_110543) do
     t.float "rating"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_dogs_on_user_id"
